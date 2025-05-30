498c62554d79a67b84f44d6e2cdffb4cce253718
claudenunc <157945959+claudenunc@users.noreply.github.com>
2025-05-30 05:06:27 -0500

    Cleanup before history rewrite

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
   },
   images: {
     domains: ['res.cloudinary.com'],
